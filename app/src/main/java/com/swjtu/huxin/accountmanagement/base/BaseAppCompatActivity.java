8fbbe06cece1ec6beb14c1de08888bdf73ed59a9
huxin9118 <332952915@qq.com>
2017-05-11 23:09:08 +0800

    fix windowTranslucentStatus in v19 and v21

--- app/src/main/java/com/swjtu/huxin/accountmanagement/base/BaseAppCompatActivity.java
+++ app/src/main/java/com/swjtu/huxin/accountmanagement/base/BaseAppCompatActivity.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
@@ -29,10 +30,11 @@ public class BaseAppCompatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         String myTheme = MyApplication.getApplication().getMyTheme();
         int themeResID = getResources().getIdentifier(myTheme, "style", getPackageName());
         setTheme(themeResID);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     @Override
